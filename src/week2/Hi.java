96dd5258f9a35fdc0bd458d733773cb2142c3bc2
Dedeepya Chowdary <Dedeepya Chowdary@LAPTOP-UNTG5G5S>
2018-11-10 23:08:23 -0500

    varaibles

---
+++ src/week2/Hi.java
@@ -0,0 +1,29 @@
+package week2;
+
+public class Hi {
+	int a = 12;
+	  int b = 14;
+	  
+	  static int m=26;
+	  static int n=27;
+	  
+	  void add()
+	  {
+		  int x=14;
+		  int y=16;
+		  int z=x+y;
+		  System.out.println("x+y value is " +z);
+	  }
+		  
+		  public static void main(String[] args)
+		  {
+			  Hi hi= new Hi();
+			  hi.add();
+		  }
+	}
+
+	
+
+	
+
+
